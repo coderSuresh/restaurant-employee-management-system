7b9f8042ad6be76c86291d86d1f21bfe1cf7377f
Paras Bhandari <50918320+gamedev-parash@users.noreply.github.com>
2022-07-30 15:49:57 +0545

    get id from database

--- php/add-employee.php
+++ php/add-employee.php
@@ -28,11 +28,11 @@ session_start();
             <div class="table-container">
                 <form action="insert-employee.php" method="post" class="add-form">
                     <label for="name">Name:</label>
-                    <input type="text" name="name" id="name">
+                    <input type="text" name="name" id="name" required>
                     <label for="address">Address:</label>
-                    <input type="text" name="address" id="address">
+                    <input type="text" name="address" id="address" required>
                     <label for="tel">Phone:</label>
-                    <input type="tel" name="phone" id="tel">
+                    <input type="tel" name="phone" id="tel" required>
                     <label for="department">Department:</label>
                    <select name="department" id="department">
                        <?php
--- php/insert-employee.php
+++ php/insert-employee.php
@@ -3,9 +3,7 @@ session_start();
 @include('config.php');
 if (isset($_POST['add-employee']))
 {   
-    $emp_name=mysqli_real_escape_string($conn, $_POST['name']);
-    $sql = "INSERT INTO employee values(default,'$emp_name')";
-    $result1 = mysqli_query($conn,$sql) or die("Error:Could not connect");
+    
     
     $emp_address=mysqli_real_escape_string($conn, $_POST['address']);
     $sql1 = "INSERT INTO address values(default,'$emp_address')";
@@ -14,20 +12,45 @@ if (isset($_POST['add-employee']))
     $emp_phone=mysqli_real_escape_string($conn, $_POST['phone']);
     $sql2 = "INSERT INTO contact values(default,$emp_phone)";
     $result3 = mysqli_query($conn,$sql2) or die("Error:Could not connect");    
-    
-    if(!empty($_POST['department']))
-    {
-        $dept = $_POST['department'];
-    }  
+       
+    $dep_name = mysqli_real_escape_string($conn, $_POST['department']);
+    $sql3 = "select dept_id from department where dept_name='$dep_name'";
+    mysqli_query($conn,$sql3) or die("Error");
 
-    if($result1&&$result2&&$result3)
+
+    if($result2&&$result3)
     {
         $_SESSION['employee-insert'] = "Inserted succesfully";
-         header("Location:add-employee.php");
-    }   
+         header("Location:add-employee.php");       
+         $addr_id= mysqli_insert_id($conn);//Get Id from Database
+         $phone_id= mysqli_insert_id($conn);
+         $emp_name =mysqli_real_escape_string($conn, $_POST['name']); 
+         $sql = "INSERT INTO employee(emp_id,emp_name,ct_id,addr_id) values(default,'$emp_name','".$phone_id."','".$addr_id."')";
+          
+         $result1 = mysqli_query($conn,$sql) or die("Error:Could not connect");
+    }
+    else{
+        echo "Result fail";
+    }
+
 }
 else
 {
-    header("Location:add-employee.php");
+   header("Location:add-employee.php");
 }
-?>
+
+// ?>
+// $query1= "INSERT INTO employee ( username, email,...)
+//             VALUES ('".$_POST["username"]."', ...)";
+//         if($result1 = mysql_query($query1))
+//         {
+//             $emp_id = mysql_insert_id(); // last created id by above query
+
+//             $query2= "INSERT INTO dept ( emp_id, dept_name, ...)
+//             VALUES ('".$emp_id."', '".$_POST["dept_name"]."',...)";
+
+//             if($result2 = mysql_query($query2))
+//             {
+//                 //success msg
+//             }
+//         }
