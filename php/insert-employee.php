34208133cdfe73188372e7ffb1de6a0895d20de7
Coder Suresh <sd807141@gmail.com>
2022-08-02 18:11:19 +0545

    fetch from db

--- php/insert-employee.php
+++ php/insert-employee.php
@@ -7,10 +7,12 @@ if (isset($_POST['add-employee']))
     $emp_address=mysqli_real_escape_string($conn, $_POST['address']);
     $sql1 = "INSERT INTO address values(default,'$emp_address')";
     $result2 = mysqli_query($conn,$sql1) or die("Error:Could not connect");
+    $addr_id= mysqli_insert_id($conn);//Get Id from Database
 
     $emp_phone=mysqli_real_escape_string($conn, $_POST['phone']);
     $sql2 = "INSERT INTO contact values(default,$emp_phone)";
-    $result3 = mysqli_query($conn,$sql2) or die("Error:Could not connect");    
+    $result3 = mysqli_query($conn,$sql2) or die("Error:Could not connect"); 
+    $phone_id= mysqli_insert_id($conn);
        
     $dep_name = mysqli_real_escape_string($conn, $_POST['department']);
     $sql3 = "select dept_id from department where dept_name='$dep_name'";//kun deparment ko id
@@ -25,12 +27,23 @@ if (isset($_POST['add-employee']))
     $row = mysqli_fetch_assoc($result5);
     $pos_id = $row['p_id'];
 
-    $addr_id= mysqli_insert_id($conn);//Get Id from Database
-    $phone_id= mysqli_insert_id($conn);
-    $emp_name =mysqli_real_escape_string($conn, $_POST['name']); 
-    $sql = "INSERT INTO employee values(default,'$emp_name','".$phone_id."','".$addr_id."',$dept_id,$pos_id)";     
+    // echo "<br>";
+    // echo $addr_id ."<br>";
+    
+    // echo $phone_id;
+    //     echo "<br>";
+    // echo "<br>";   
+    // echo "<br>";
+    // echo $dept_id ."<br>";
+    
+    // echo $pos_id;
+    //     echo "<br>";
+    // echo "<br>";
 
-    $result1 = mysqli_query($conn,$sql) or die("Error:Could not connect");
+    $emp_name =mysqli_real_escape_string($conn, $_POST['name']); 
+    $sql = "INSERT INTO employee values(default,'$emp_name','.$phone_id.','.$addr_id.',$dept_id,$pos_id)";     
+
+    $result1 = mysqli_query($conn,$sql) or die(mysqli_error($conn));
 
     if($result1&&$result2&&$result3&&$result4&&$result5)
     {
