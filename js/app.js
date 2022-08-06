8d7b96ff88eb9eafa7c865473b36cbf2144cb45d
Coder Suresh <sd807141@gmail.com>
2022-08-06 20:15:20 +0545

    bug fixes

--- js/app.js
+++ js/app.js
@@ -8,11 +8,15 @@ menuBtn.addEventListener("click", () => {
 })
 
 // to confirm before delete
-const deleteBtn = document.querySelector(".delete_warn")
-deleteBtn.addEventListener("click", (e) => {
-    if (!confirm("Are you sure to delete?")) {
-        e.preventDefault()
-        return false
-    }
-    else return true
+const deleteBtn = document.querySelectorAll('.delete_warn')
+deleteBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (confirm("Are you sure to delete?")) {
+            return true
+        }
+        else {
+            e.preventDefault()
+            return false
+        }
+    })
 })
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+deteleBtn.forEach(button => {
+//     button.addEventListener('click', (e) => {
+//         if (confirm("Are you sure to delete?")) {
+//             return true
+//         }
+//         else {
+//             e.preventDefault()
+//             return false
+//         }
+//     })
+// })
