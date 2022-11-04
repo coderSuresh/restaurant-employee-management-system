7f27db9823d916592a3d29f26bd9208ea5e33b9d
coderSuresh <sd807141@gmail.com>
2022-11-04 08:28:40 +0545

    don't do previous commit

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ const menuBtn = document.querySelector(".sidebar-menu")
 const sidebar = document.querySelector(".sidebar")
 const body = document.querySelector(".body")
 
-sidebar.classList.add("open")
-body.classList.add("open")
+// sidebar.classList.add("open")
+// body.classList.add("open")
 
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("open")
