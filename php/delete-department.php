602768a1f643cb6e7deda16c59cae6bb0ea3de76
coderSuresh <sd807141@gmail.com>
2022-11-05 15:37:13 +0545

    fix position delete

--- php/delete-department.php
+++ php/delete-department.php
@@ -5,15 +5,20 @@
         $dept_id = $_POST['dept_id'];
         $sql = "delete from department where dept_id=$dept_id";
         
-        $sql_check = "select position.p_id,
+     //    check associated employees
+        $sql_check_emp = "select position.p_id,
                              employee.emp_id
                              from employee
                              inner join position on employee.p_id = position.p_id
                              where employee.dept_id = $dept_id";
         
-        $res_check = mysqli_query($conn, $sql_check) or die("Could not fetch from database");
+        $res_check = mysqli_query($conn, $sql_check_emp) or die("Could not fetch from database");
 
-       if(mysqli_num_rows($res_check) > 0) {
+     // check associated positions
+     $sql_check_pos = "select p_id from position where dept_id = $dept_id";
+     $res_check_pos = mysqli_query($conn, $sql_check_pos) or die("Could not fetch from database");
+
+       if(mysqli_num_rows($res_check) > 0 || mysqli_num_rows($res_check_pos) > 0) {
             $_SESSION['non-empty-dept'] = "Can't delete department!";
             header("location:./departments.php");
        }
